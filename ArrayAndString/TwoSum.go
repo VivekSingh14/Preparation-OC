cf510eff4540979c359a530e67931f24a54d36f6
Vivek Singh <vivek.singh141@yahoo.com>
2024-11-09 22:37:37 +0530

    Init config (#2)
    * feat: added two sum

--- ArrayAndString/TwoSum.go
+++ ArrayAndString/TwoSum.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Checking Github actions and basic configurations")
+	arr := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(arr, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	var res []int
+	hashmap := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		if value, ok := hashmap[target-nums[i]]; ok {
+			res = append(res, value)
+			res = append(res, i)
+		}
+		hashmap[nums[i]] = i
+	}
+	return res
 }
